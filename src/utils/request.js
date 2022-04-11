c7b0256dabd4522febe02605fdb2f402893dd433
activationeuron <aforamitrai@gmail.com>
2022-04-11 22:02:31 +0530

    added request

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const url =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:8000/api' : '/api';
+  process.env.NODE_ENV === 'development'
+    ? 'http://172.105.156.217:8000/api'
+    : '/api';
 
 const request = axios.create({ baseURL: url });
 
