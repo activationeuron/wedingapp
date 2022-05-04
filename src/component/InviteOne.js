caa8adc512b6a2e9302b836a1e250db2bd1e4e36
Your Name <you@example.com>
2022-05-04 20:16:44 +0000

    delete

--- src/component/InviteOne.js
+++ src/component/InviteOne.js
@@ -58,6 +58,7 @@ function InviteOne() {
       });
       if (response.data.success) {
         alert('INVITATION SUCCESS');
+        setShow(false)
         getAllIncitations();
       }
     } catch (error) {
@@ -149,7 +150,7 @@ function InviteOne() {
             {all &&
               all.map((guest) => {
                 return (
-                  <div className='border-2 h-40  my-2 py-2 px-2'>
+                  <div className='border-2 h-40  my-2 py-2 px-2 relative'>
                     <div className='flex justify-between'>
                       <div>
                         <div className='text-sm  text-gray-800'>
@@ -174,6 +175,10 @@ function InviteOne() {
                         </div>
                       </div>
                     </div>
+
+                    <div className='absolute bottom-1' onClick={()=>deleteEntry(guest._id)}><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+</svg></div>
                   </div>
                 );
               })}
