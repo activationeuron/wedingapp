2a1298f1a858e9dd19dc7d3e5a15700f9c358d13
activationeuron <aforamitrai@gmail.com>
2022-05-07 22:46:17 +0530

    build

---
+++ build/precache-manifest.889855b697149e0be6e170eb8a857a4d.js
@@ -0,0 +1,370 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941d8dc96668de460b7b9a6c4b9f7c1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c29baad6155e0490a70e",
+    "url": "/static/css/2.0ea65fe4.chunk.css"
+  },
+  {
+    "revision": "1968d8325aacf8e45a45",
+    "url": "/static/css/main.d141df6d.chunk.css"
+  },
+  {
+    "revision": "c29baad6155e0490a70e",
+    "url": "/static/js/2.464ad267.chunk.js"
+  },
+  {
+    "revision": "6d4166b704bad8536822d1a558161b7a",
+    "url": "/static/js/2.464ad267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1968d8325aacf8e45a45",
+    "url": "/static/js/main.c931497d.chunk.js"
+  },
+  {
+    "revision": "350c8ba1c1b716f60c47",
+    "url": "/static/js/runtime-main.3a833890.js"
+  },
+  {
+    "revision": "bf18c7187d2f85f0fe602ee403a7c859",
+    "url": "/static/media/1.bf18c718.png"
+  },
+  {
+    "revision": "6f4bb99c1cfc85909bdfe46aee8e4076",
+    "url": "/static/media/10.6f4bb99c.png"
+  },
+  {
+    "revision": "4cefc7a224cb374b94dbe32577baf8cc",
+    "url": "/static/media/11.4cefc7a2.png"
+  },
+  {
+    "revision": "5f0691bf681dfc264a2f4308d11c4701",
+    "url": "/static/media/12.5f0691bf.png"
+  },
+  {
+    "revision": "e6c02a5fc55b1284079b7cff29c28fab",
+    "url": "/static/media/14.e6c02a5f.png"
+  },
+  {
+    "revision": "2ccc99f6b520fa1e365dbc7d9d727368",
+    "url": "/static/media/15.2ccc99f6.png"
+  },
+  {
+    "revision": "d927f4bbfd7373b0bd7fabe1c25cd331",
+    "url": "/static/media/16.d927f4bb.png"
+  },
+  {
+    "revision": "ceefc0367bc8abe285c40a8539506c7b",
+    "url": "/static/media/17.ceefc036.png"
+  },
+  {
+    "revision": "dc88537017fc142f14c05eea29dcb22a",
+    "url": "/static/media/18.dc885370.png"
+  },
+  {
+    "revision": "cea8cc759ffec80e1285fc8aa52e0944",
+    "url": "/static/media/19.cea8cc75.png"
+  },
+  {
+    "revision": "76f3b3c5f3f5799cfbbbb18f103f60bc",
+    "url": "/static/media/2.76f3b3c5.png"
+  },
+  {
+    "revision": "23d6db314d7070d77a8fdb773f6b9d67",
+    "url": "/static/media/20.23d6db31.png"
+  },
+  {
+    "revision": "008d46d2ddaa057aa9584a77a8fd1f81",
+    "url": "/static/media/21.008d46d2.png"
+  },
+  {
+    "revision": "bc75211c892390104a9c456e9946c176",
+    "url": "/static/media/22.bc75211c.png"
+  },
+  {
+    "revision": "c89beaad20cfa9b4fc66a60b934558b3",
+    "url": "/static/media/23.c89beaad.png"
+  },
+  {
+    "revision": "42bdc670ae3c74c2ba8e6c880efe4581",
+    "url": "/static/media/24.42bdc670.png"
+  },
+  {
+    "revision": "dd0a6ea2c234e1629d535bf3c7dbcbd2",
+    "url": "/static/media/25.dd0a6ea2.png"
+  },
+  {
+    "revision": "17b3d5e6d33a33d3fef4c3d36aef7aec",
+    "url": "/static/media/26.17b3d5e6.png"
+  },
+  {
+    "revision": "45a88b21a45eb9b68f4a1d841739bfd2",
+    "url": "/static/media/27.45a88b21.png"
+  },
+  {
+    "revision": "15b9e45d262a7683b2646a88c40f6f63",
+    "url": "/static/media/28.15b9e45d.png"
+  },
+  {
+    "revision": "e98781ae7e90d42837d193d4da2a259a",
+    "url": "/static/media/29.e98781ae.png"
+  },
+  {
+    "revision": "69c53a2354a7f2bb798181749c2be93e",
+    "url": "/static/media/3.69c53a23.png"
+  },
+  {
+    "revision": "a115ca06c3a3634afe5d57afc2e0d4de",
+    "url": "/static/media/30.a115ca06.png"
+  },
+  {
+    "revision": "6890dea814783a5bc2986a365d198f79",
+    "url": "/static/media/31.6890dea8.png"
+  },
+  {
+    "revision": "4e38a3463348914c6d97054dfec43981",
+    "url": "/static/media/32.4e38a346.png"
+  },
+  {
+    "revision": "cb3ca0e5785fab5ab600e7e42320b84f",
+    "url": "/static/media/33.cb3ca0e5.png"
+  },
+  {
+    "revision": "c1413bc4e8b4b18997fc357fb3546940",
+    "url": "/static/media/34.c1413bc4.png"
+  },
+  {
+    "revision": "1c2b80e401165af4f0de47b7e7e94193",
+    "url": "/static/media/35.1c2b80e4.jpeg"
+  },
+  {
+    "revision": "2527fff3378828c3395e17ec389c3a8b",
+    "url": "/static/media/36.2527fff3.jpeg"
+  },
+  {
+    "revision": "9c17c299cd25eca394e60a098bf44cd3",
+    "url": "/static/media/37.9c17c299.jpg"
+  },
+  {
+    "revision": "f72177de94171957dfc6cbd83f172cde",
+    "url": "/static/media/38.f72177de.jpg"
+  },
+  {
+    "revision": "18683aec58b7c0a6887a53272a3da24a",
+    "url": "/static/media/39.18683aec.png"
+  },
+  {
+    "revision": "c0f83127042a18a5eb07e6f66171b1e3",
+    "url": "/static/media/4.c0f83127.png"
+  },
+  {
+    "revision": "1a2a38e1cd0c5a913665d7da22b450ac",
+    "url": "/static/media/40.1a2a38e1.png"
+  },
+  {
+    "revision": "ab6f1675d5e2287c0970bf19578e4eea",
+    "url": "/static/media/41.ab6f1675.png"
+  },
+  {
+    "revision": "6e3d13cd47b9bfcffd88aeb99041776f",
+    "url": "/static/media/42.6e3d13cd.png"
+  },
+  {
+    "revision": "761df412d1a33c9309af9cf2c12a5b07",
+    "url": "/static/media/5.761df412.png"
+  },
+  {
+    "revision": "76a8fffc45bec5afd3c455c11a2c0012",
+    "url": "/static/media/6.76a8fffc.png"
+  },
+  {
+    "revision": "b8fdda57c29e032097f3c3e041ce06bd",
+    "url": "/static/media/7.b8fdda57.png"
+  },
+  {
+    "revision": "fc2d7d1af27f8f8cc22c8562b0e37ef8",
+    "url": "/static/media/8.fc2d7d1a.png"
+  },
+  {
+    "revision": "018870ab14b47f32de89568d72be3bc5",
+    "url": "/static/media/9.018870ab.png"
+  },
+  {
+    "revision": "f2cd92ec077703dbbfcb4314338c0dc3",
+    "url": "/static/media/afaan-mariam-logo.f2cd92ec.png"
+  },
+  {
+    "revision": "7bb706cd30dc5f0f775ce1f93697ca5c",
+    "url": "/static/media/afaan.7bb706cd.png"
+  },
+  {
+    "revision": "16e1654722dd469197c9015181ee0389",
+    "url": "/static/media/banner-decoration.16e16547.png"
+  },
+  {
+    "revision": "654af2165d941a128a1b06389106ee39",
+    "url": "/static/media/banner.654af216.mp4"
+  },
+  {
+    "revision": "45e59b5e12eceab831acc983bae2a833",
+    "url": "/static/media/bg.45e59b5e.jpg"
+  },
+  {
+    "revision": "38e20216a07fd4baffd56b108601ae93",
+    "url": "/static/media/breadcrumb.38e20216.jpg"
+  },
+  {
+    "revision": "123b7d37cd8d85017b593ad45c328a13",
+    "url": "/static/media/close.123b7d37.svg"
+  },
+  {
+    "revision": "396850a1ca4351524bd9fa8aca550274",
+    "url": "/static/media/danyal-mohammed-photo-2.396850a1.png"
+  },
+  {
+    "revision": "7f10a2e2701ebcd54b5b07a94c85cd79",
+    "url": "/static/media/danyal-mohammed-photo.7f10a2e2.png"
+  },
+  {
+    "revision": "0c127e1f8edff85e3bbe888efb256b9c",
+    "url": "/static/media/flaticon.0c127e1f.woff"
+  },
+  {
+    "revision": "245ef08dbd1654c4de52ec75349595ae",
+    "url": "/static/media/flaticon.245ef08d.woff2"
+  },
+  {
+    "revision": "54603b3dabe009017899c36c3a1758d7",
+    "url": "/static/media/flaticon.54603b3d.eot"
+  },
+  {
+    "revision": "dcf65c4f32fb0fb9382f400de5a6aa3d",
+    "url": "/static/media/flaticon.dcf65c4f.ttf"
+  },
+  {
+    "revision": "e958c5f1ae25f44f53062aaba546e178",
+    "url": "/static/media/flaticon.e958c5f1.svg"
+  },
+  {
+    "revision": "04b71bec9cef872e26ce7bc00b9dd0cc",
+    "url": "/static/media/fontawesome-webfont.04b71bec.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "56c2f01c8fa9b0a1baa8ddc760f7ff03",
+    "url": "/static/media/gallery-18.56c2f01c.png"
+  },
+  {
+    "revision": "7cc4b5fa39fe5b3e55aa62e2d0d84fb0",
+    "url": "/static/media/gallery-2.7cc4b5fa.png"
+  },
+  {
+    "revision": "0462ad4eead6cb825c8e085529ac010e",
+    "url": "/static/media/gallery-4.0462ad4e.png"
+  },
+  {
+    "revision": "9223b06cb10135e4e9fda579d4fd50fe",
+    "url": "/static/media/gallery-5.9223b06c.png"
+  },
+  {
+    "revision": "6206ab1f5c0edea18fe99f17d85654d7",
+    "url": "/static/media/gallery-7.6206ab1f.png"
+  },
+  {
+    "revision": "e7815b789adc8602afa35836801d5958",
+    "url": "/static/media/gallery-9.e7815b78.png"
+  },
+  {
+    "revision": "b0164d10dfa09e15d773b7a61877d9ce",
+    "url": "/static/media/gallery-banner.b0164d10.jpg"
+  },
+  {
+    "revision": "52c24ca2fd48bc7c78d97cf230690395",
+    "url": "/static/media/haldi-ceremony.52c24ca2.mp4"
+  },
+  {
+    "revision": "8ad3706bde7e2d905c118a84144de578",
+    "url": "/static/media/mariam.8ad3706b.png"
+  },
+  {
+    "revision": "7a285878b4eaac8f3622b82168853329",
+    "url": "/static/media/marriage-ceremony.7a285878.mp4"
+  },
+  {
+    "revision": "e6f1329801f1a78b5ea52290071a9b8f",
+    "url": "/static/media/maternal-grandfather.e6f13298.jpeg"
+  },
+  {
+    "revision": "b125285ac863c6df18333acae4e86816",
+    "url": "/static/media/maternal-grandmother.b125285a.jpg"
+  },
+  {
+    "revision": "7bc2fe8fd073addbd1f4484852d67d28",
+    "url": "/static/media/mohsi-mohammed.7bc2fe8f.png"
+  },
+  {
+    "revision": "8d741c63487b1cc7e2035a64d3060042",
+    "url": "/static/media/parallax-image.8d741c63.png"
+  },
+  {
+    "revision": "ceb60f341a9bd1e1fccd846c32bff431",
+    "url": "/static/media/paternal-grandmother.ceb60f34.png"
+  },
+  {
+    "revision": "d8b2ded1d90ce0014e2c0c42fed6f52e",
+    "url": "/static/media/reception.d8b2ded1.mp4"
+  },
+  {
+    "revision": "6a5f37cc5760d54cff5679d3c118834c",
+    "url": "/static/media/reema-khan-photo.6a5f37cc.png"
+  },
+  {
+    "revision": "2a45133e2727a07d22bc61aa3ee7090b",
+    "url": "/static/media/sangeet-ceremony.2a45133e.mp4"
+  },
+  {
+    "revision": "6b209fef80d8e674ef2f28bc9f8843e3",
+    "url": "/static/media/section-title.6b209fef.png"
+  },
+  {
+    "revision": "45b29c77e67e306e2202509cc09596d7",
+    "url": "/static/media/shape2.45b29c77.png"
+  },
+  {
+    "revision": "4a8405343d535e0a60d62d57e95a0563",
+    "url": "/static/media/slide-10.4a840534.jpg"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/static/media/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/static/media/themify.e23a7dca.ttf"
+  }
+]);
